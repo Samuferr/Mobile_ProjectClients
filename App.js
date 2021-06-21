464aa9827b3cf1ad1fc420d39fba7705bc3aab82
Samuferr <samuferr@gmail.com>
2021-06-21 18:28:39 -0300

    Created Project and Stack Navigation

--- App.js
+++ App.js
@@ -1,21 +1,55 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { Text, View } from 'react-native'
+import {NavigationContainer} from '@react-navigation/native'
+import {createStackNavigator} from '@react-navigation/stack'
+import {Button, Icon} from 'react-native-elements'
+import UserList from './src/views/UserList'
+import UserForm from './src/views/UserForm'
 
-export default function App() {
+
+const Stack = createStackNavigator()
+
+export default props => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+   <NavigationContainer>
+     <Stack.Navigator 
+        initialRouteName="UserList"
+        screenOptions={screenOptions}
+        >
+       <Stack.Screen
+          name="UserList"
+          component={UserList}
+          options={({navigation}) => {
+            return {
+            title:"Formulário de Usuários",
+            headerRight: () => (
+              <Button 
+                onPress={() => navigation.navigate("UserForm")}
+                type= 'clear'
+                icon= {<Icon name="add" size={25} color="white"/>}
+              />
+            )
+            }
+          }}
+        />
+       <Stack.Screen 
+          name="UserForm"
+          component={UserForm}
+          options={{
+            title:"Formulário de Usuários"
+          }}
+       />
+     </Stack.Navigator>
+   </NavigationContainer>
+)
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const screenOptions = {
+  headerStyle: {
+    backgroundColor: '#f4511e'
   },
-});
+  headerTintColor: '#fff',
+   headerTitleStyle: {
+     fontWeight: 'bold'
+   }
+}
