75fd27e9941a93938c004c6892964dee5300e028
Samuferr <samuferr@gmail.com>
2021-06-21 19:12:57 -0300

    Created FlatList by Users

--- src/views/UserList.js
+++ src/views/UserList.js
@@ -1,10 +1,30 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import { Text, View, FlatList } from 'react-native'
+import { ListItem } from 'react-native-elements'
+import users from './../data/users'
 
 export default props => {
+
+    console.warn(Object.keys(props))
+
+    function getUserItem( {item} ) {
+        return (
+            <ListItem 
+                key={user.id}
+                title={user.name}
+                subtitle={user.email}
+                onPress={() => props.navigation.navigate('UserForm')}
+            />
+        )
+    }
+
     return (
         <View>
-            <Text>UserList</Text>
+            <FlatList
+                keyExtractor={user => user.id.toString()}
+                data = {users}    
+                renderItem={getUserItem}
+            />
         </View>
 )
 }
